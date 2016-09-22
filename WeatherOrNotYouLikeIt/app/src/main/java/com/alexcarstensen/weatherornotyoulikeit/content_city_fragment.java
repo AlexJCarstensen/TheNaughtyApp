2a49d39573df27b76752a65097ee554d900c1552
Zwrah <201370801@iha.dk>
2016-09-22 12:04:16 +0200

    Fragment layout ok
    Super!

---
+++ WeatherOrNotYouLikeIt/app/src/main/java/com/alexcarstensen/weatherornotyoulikeit/content_city_fragment.java
@@ -0,0 +1,20 @@
+package com.alexcarstensen.weatherornotyoulikeit;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class content_city_fragment extends Fragment {
+    public content_city_fragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_fragment_city,
+                container, false);
+    }
+}
---
+++ WeatherOrNotYouLikeIt/app/src/main/java/com/alexcarstensen/weatherornotyoulikeit/content_history_weather_fragment.java
@@ -0,0 +1,21 @@
+package com.alexcarstensen.weatherornotyoulikeit;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class content_history_weather_fragment extends Fragment {
+
+    public content_history_weather_fragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.content_fragment_weather_history,
+                container, false);
+    }
+}
