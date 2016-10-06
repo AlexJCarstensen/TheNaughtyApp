42c981a2ff66525678bb8b1b38ebad122b77f0e0
AlexCarstensen <alex.jul.carstensen@gmail.com>
2016-10-06 10:02:04 +0200

    added click for maps

--- TheBrandApp/app/src/main/java/com/alexcarstensen/thebrandapp/MainActivity.java
+++ TheBrandApp/app/src/main/java/com/alexcarstensen/thebrandapp/MainActivity.java
@@ -1,18 +1,12 @@
 package com.alexcarstensen.thebrandapp;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -54,8 +48,11 @@ public class MainActivity extends AppCompatActivity implements MainContactListFr
         mapFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Map!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                Intent mapIntent = new Intent(getApplicationContext(), MapActivity.class);
+
+                startActivity(mapIntent);
+
             }
         });
 
